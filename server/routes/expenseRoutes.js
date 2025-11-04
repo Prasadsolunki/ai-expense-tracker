30d05aba855446ed80363e8e087d39a45050f415
Prasad <solunkiprasad1305@gmail.com>
2025-11-04 23:24:50 +0530

    Admin api for register and login

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { addExpense, getExpenses } from "../controllers/ExpenseController.js";
+import { protect } from "../src/middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", addExpense);
-router.get("/", getExpenses);
+router.post("/", protect, addExpense);
+router.get("/", protect, getExpenses);
 
 export default router;
